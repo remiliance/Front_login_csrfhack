bbaed5250aa613088b20e854ea7a8b13dba3c8e0
RemiPoly <remiliance@yahoo.fr>
2023-08-28 11:27:26 +0200

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      csrf: "",
+      username: "",
+      password: "",
+      error: "",
+      isAuthenticated: false,
+    };
+  }
+
+
+   tiger = () => {
+    fetch("http://zoo.com:8000/auth_app/csrf/", {
+      //credentials: "include",
+    })
+    .then(this.isResponseOk)
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
+
+  render() {
+    return (
+      <div className="container mt-3">
+        <h1>React Cookie CSRF Hack </h1>
+        <button className="btn btn-primary mr-3"  onClick={this.tiger}>GET_CSRF_8000</button>
+        <form action="http://zoo.com:8000/api/tigernoscrf/test_tiger_post_nocsrf/" method="post">
+        <input type="submit" name="btnSubmit" value="Go_WT_CSRF" />
+        </form>
+        <form action="http://zoo.com:8000/api/tiger/test_tiger_post/" method="post">
+        <input type="submit" name="btnSubmit" value="Go_W_CSRF" />
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App;
